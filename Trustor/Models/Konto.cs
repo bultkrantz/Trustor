f458ebd3afec50fd08fbadad5e824b883c7d4131
mattias sjöden <mattias.nackademin@gmail.com>
2017-11-01 11:38:07 +0100

    Add Class Konto to Models

---
+++ Trustor/Models/Konto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trustor.Models
+{
+    class Konto
+    {
+        public int Kontonummer { get; set; }
+        public int Kundnummer { get; set; }
+        public decimal Saldo { get; set; }
+
+    }
+}

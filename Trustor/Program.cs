c3acdcbd3c5cda323a4699b2cde8185198c7bd8a
Marcus Hultkrantz <marcus.hultkrantz@live.se>
2017-11-01 10:31:17 +0100

    Add project files.

---
+++ Trustor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trustor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

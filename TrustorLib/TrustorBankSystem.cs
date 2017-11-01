00cdef934e2ff714f1433e58c343f453b424a52b
Marcus Hultkrantz <marcus.hultkrantz@live.se>
2017-11-01 19:56:35 +0100

    Add class TrustorBankSystem

---
+++ TrustorLib/TrustorBankSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrustorLib.Models;
+
+namespace TrustorLib
+{
+    public class TrustorBankSystem
+    {
+        public List<Customer> SearchCustomer(string name)
+        {
+            throw new NotImplementedException();
+        }
+        public Customer ShowCustomerInfo(int customerNumber)
+        {
+            throw new NotImplementedException();
+        }
+        public Customer CreateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteCustomer(int customerNumber)
+        {
+            throw new NotImplementedException();
+        }
+        public Account CreateAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteAccount(int accountNumber)
+        {
+            throw new NotImplementedException();
+        }
+        public void NewDeposit(int accountNumber, decimal deposit)
+        {
+            throw new NotImplementedException();
+        }
+        public void NewWithdrawal(int accountNumber, decimal withdrawal)
+        {
+            throw new NotImplementedException();
+        }
+        public void NewTransfer(int fromAccountNumber, int toAccountNumber, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

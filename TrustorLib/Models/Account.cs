9f5b17d8aa4cb87767cefe39a5d0550ac177c552
Marcus Hultkrantz <marcus.hultkrantz@live.se>
2017-11-01 12:54:41 +0100

    Created new project TrustorLib and deleted models in Trustor

---
+++ TrustorLib/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace TrustorLib.Models
+{
+    public class Account
+    {
+        public int AccountNumber { get; set; }
+        public int CustomerNumber { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
